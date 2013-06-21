0bd5d29a4dd45a4604eb2d5703d55e44ed7820bd
Jerry Goodnough <ferret@stomrwoods.com>
2013-06-21 11:12:06 -0700

    Minor reformatting

--- src/main/java/org/socraticgrid/codeconversion/exceptions/InitializationException.java
+++ src/main/java/org/socraticgrid/codeconversion/exceptions/InitializationException.java
@@ -1,34 +1,31 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in the
+ * editor.
  */
 package org.socraticgrid.codeconversion.exceptions;
 
 /**
- *
- * @author Jerry Goodnough
+ * @author  Jerry Goodnough
  */
 public class InitializationException extends Exception
 {
 
     /**
-     *
-     * @param cause
+     * @param  cause
      */
 
     public InitializationException(Throwable cause)
     {
-         super(cause);
+        super(cause);
     }
 
     /**
-     *
-     * @param message
-     * @param cause
+     * @param  message
+     * @param  cause
      */
 
-    public InitializationException(String message,Throwable cause)
+    public InitializationException(String message, Throwable cause)
     {
-         super(message,cause);
+        super(message, cause);
     }
 }
