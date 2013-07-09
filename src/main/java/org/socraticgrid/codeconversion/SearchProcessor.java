7cba0ac6dab381f0dbc621d95bb03a493c9a5072
Jerry Goodnough <ferret@stomrwoods.com>
2013-07-08 19:39:05 -0700

    Added CodeSearch based public interface
    Add findCodes and findCode based on CodeSearch. Reformatted Text

--- src/main/java/org/socraticgrid/codeconversion/SearchProcessor.java
+++ src/main/java/org/socraticgrid/codeconversion/SearchProcessor.java
@@ -42,127 +42,103 @@
 package org.socraticgrid.codeconversion;
 
 import com.google.common.cache.Cache;
-import java.util.LinkedList;
-import java.util.List;
+import com.google.common.cache.CacheBuilder;
+
 import org.socraticgrid.codeconversion.elements.CodeReference;
 import org.socraticgrid.codeconversion.elements.CodeSearch;
-import com.google.common.cache.CacheBuilder;
+import org.socraticgrid.codeconversion.elements.NullCodeReference;
+import org.socraticgrid.codeconversion.elements.SearchOptions;
+
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import org.socraticgrid.codeconversion.elements.NullCodeReference;
-import org.socraticgrid.codeconversion.elements.SearchOptions;
+
 import javax.annotation.PostConstruct;
 
 
 /**
- * Master Search Processor class that will manage multiple search pipelines.
- * Also handles caching of results.
+ * Master Search Processor class that will manage multiple search pipelines. Also
+ * handles caching of results.
  *
- * @author Jerry Goodnough
+ * @author  Jerry Goodnough
  */
-
 public class SearchProcessor
 {
-    private List<SearchPipeline> pipeLineList;
-    private Cache<CodeSearch, List<CodeReference>> searchCache = null;
-    private boolean onSearchFailUseOrginal = false;
-    private boolean searchAllPipelines = false;
     private int cacheSize = 128;
     private HashMap<String, String> codeTargetMap = new HashMap<String, String>();
+    private boolean onSearchFailUseOrginal = false;
+    private List<SearchPipeline> pipeLineList;
+    private boolean searchAllPipelines = false;
+    private Cache<CodeSearch, List<CodeReference>> searchCache = null;
 
     /**
-     *
+     * Creates a new SearchProcessor object.
      */
     public SearchProcessor()
     {
     }
 
     /**
+     * Find match code using a CodeSearch Object.
      *
-     * @return
-     */
-    public List<SearchPipeline> getPipeLineList()
-    {
-        return this.pipeLineList;
-    }
-
-    /**
+     * @param   find
      *
-     * @param list
+     * @return  A Single Code Reference
      */
-    public void setPipeLineList(List<SearchPipeline> list)
-    {
-        this.pipeLineList = list;
-    }
-    /**
-     *
-     * @param cacheSize
-     */
-    public void setCacheSize(int cacheSize)
-    {
-        this.cacheSize = cacheSize;
-    }
-
-    /**
-     *
-     */
-    @PostConstruct
-    public void initialize()
-    {
-        searchCache = CacheBuilder.newBuilder().maximumSize(cacheSize).expireAfterWrite(10, TimeUnit.MINUTES).build();
-    }
-
-    /*
-     public static CodeReference getCodeReference(String targetSystem, String sourceSystem, String sourceCode, String sourceText)
-     {
-     return getSerchProcessor().findCode(targetSystem, sourceSystem, sourceCode, sourceText);
-
-     }
-     */
-    /**
-     *
-     * @param sourceSystem
-     * @param sourceCode
-     * @param sourceText
-     * @return
-     */
-    public CodeReference translateCode(String sourceSystem, String sourceCode, String sourceText)
-    {
-        CodeReference out;
-        if (codeTargetMap.containsKey(sourceSystem))
-        {
-            out = this.findCode(codeTargetMap.get(sourceSystem), sourceSystem, sourceCode, sourceText);
-        }
-        else
-        {
-            //Refelect back out what we have.
-            out = new CodeReference(sourceSystem, sourceCode, sourceText);
-        }
-        return out;
-    }
-
-    /**
-     *
-     * @param targetSystem
-     * @param sourceSystem
-     * @param sourceCode
-     * @param sourceText
-     * @return
-     */
-    public CodeReference findCode(String targetSystem, String sourceSystem, String sourceCode, String sourceText)
+    public CodeReference findCode(CodeSearch find)
     {
         CodeReference result;
+        List<CodeReference> found = this.findCodes(find);
 
-        List<CodeReference> found = this.findCodes(targetSystem, sourceSystem, sourceCode);
         if (!found.isEmpty())
         {
             result = found.get(0);
         }
         else
         {
+
+            if (onSearchFailUseOrginal)
+            {
+                result = new CodeReference(find.getSystem(), find.getCode(),
+                        find.getDisplay());
+            }
+            else
+            {
+                result = NullCodeReference.getNullCode();
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   targetSystem
+     * @param   sourceSystem
+     * @param   sourceCode
+     * @param   sourceText
+     *
+     * @return
+     */
+    public CodeReference findCode(String targetSystem, String sourceSystem,
+        String sourceCode, String sourceText)
+    {
+        CodeReference result;
+        List<CodeReference> found = this.findCodes(targetSystem, sourceSystem,
+                sourceCode);
+
+        if (!found.isEmpty())
+        {
+            result = found.get(0);
+        }
+        else
+        {
+
             if (onSearchFailUseOrginal)
             {
                 result = new CodeReference(sourceSystem, sourceCode, sourceText);
@@ -172,26 +148,30 @@ public class SearchProcessor
                 result = NullCodeReference.getNullCode();
             }
         }
+
         return result;
     }
 
     /**
+     * DOCUMENT ME!
      *
-     * @param targetSystem
-     * @param sourceText
+     * @param   targetSystem
+     * @param   sourceText
+     *
      * @return
      */
     public CodeReference findCodeByText(String targetSystem, String sourceText)
     {
         CodeReference result;
-
         List<CodeReference> found = this.findCodesByText(targetSystem, sourceText);
+
         if (!found.isEmpty())
         {
             result = found.get(0);
         }
         else
         {
+
             if (onSearchFailUseOrginal)
             {
                 result = new CodeReference("", "", sourceText);
@@ -201,26 +181,25 @@ public class SearchProcessor
                 result = NullCodeReference.getNullCode();
             }
         }
-        return result;
 
+        return result;
     }
 
-    ;
     /**
+     * Find match codes using a CodeSearch Object.
      *
-     * @param targetSystem
-     * @param sourceSystem
-     * @param sourceCode
-     * @return
+     * @param   find
+     *
+     * @return  List of Matching Codes
      */
-    public List<CodeReference> findCodes(String targetSystem, String sourceSystem, String sourceCode)
+    public List<CodeReference> findCodes(CodeSearch find)
     {
         List<CodeReference> result = new LinkedList<CodeReference>();
 
-        CodeSearch find = new CodeSearch(sourceSystem, sourceCode, "", targetSystem);
         try
         {
-            result = searchCache.get(find, new PipelineSearcher(find, pipeLineList, searchAllPipelines));
+            result = searchCache.get(find,
+                    new PipelineSearcher(find, pipeLineList, searchAllPipelines));
         }
         catch (ExecutionException e)
         {
@@ -230,23 +209,54 @@ public class SearchProcessor
     }
 
     /**
+     * DOCUMENT ME!
      *
-     * @param targetSystem
-     * @param sourceText
+     * @param   targetSystem
+     * @param   sourceSystem
+     * @param   sourceCode
+     *
      * @return
      */
-    public List<CodeReference> findCodesByText(String targetSystem, String sourceText)
+    public List<CodeReference> findCodes(String targetSystem, String sourceSystem,
+        String sourceCode)
     {
         List<CodeReference> result = new LinkedList<CodeReference>();
+        CodeSearch find = new CodeSearch(sourceSystem, sourceCode, "", targetSystem);
 
+        try
+        {
+            result = searchCache.get(find,
+                    new PipelineSearcher(find, pipeLineList, searchAllPipelines));
+        }
+        catch (ExecutionException e)
+        {
+        }
+
+        return result;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   targetSystem
+     * @param   sourceText
+     *
+     * @return
+     */
+    public List<CodeReference> findCodesByText(String targetSystem,
+        String sourceText)
+    {
+        List<CodeReference> result = new LinkedList<CodeReference>();
         CodeSearch find = new CodeSearch();
         find.setDisplay(sourceText);
         find.setTargetSystem(targetSystem);
-        find.setSearchType(SearchOptions.ANY_Code + SearchOptions.LITERAL_TargetSystem + SearchOptions.LITERAL_Display);
+        find.setSearchType(SearchOptions.ANY_Code +
+            SearchOptions.LITERAL_TargetSystem + SearchOptions.LITERAL_Display);
 
         try
         {
-            result = searchCache.get(find, new PipelineSearcher(find, pipeLineList, searchAllPipelines));
+            result = searchCache.get(find,
+                    new PipelineSearcher(find, pipeLineList, searchAllPipelines));
         }
         catch (ExecutionException e)
         {
@@ -255,53 +265,31 @@ public class SearchProcessor
         return result;
     }
 
-    class PipelineSearcher implements Callable<List<CodeReference>>
-    {
-
-        CodeSearch find;
-        List<SearchPipeline> pipelines;
-        boolean searchAllPipelines;
-
-        PipelineSearcher(CodeSearch find, List<SearchPipeline> pipelines, boolean searchAllPipelines)
-        {
-            this.find = find;
-            this.pipelines = pipelines;
-            this.searchAllPipelines = searchAllPipelines;
-
-        }
-
-        @Override
-        public List<CodeReference> call()
-        {
-            LinkedList<CodeReference> out = new LinkedList<CodeReference>();
-
-            Iterator<SearchPipeline> itr = pipelines.iterator();
-            while (itr.hasNext())
-            {
-                SearchPipeline sp = itr.next();
-                if (sp.SupportsTarget(find))
-                {
-                    sp.match(find, out);
-                }
-
-            }
-            return out;
-        }
-    }
-
     /**
-     * Controls if just the first pipeline to find a match should be consulted.
+     * Determine if the original code value is returned as the result on a failed
+     * search.
      *
-     * @param bool true if all pipelines should be searched for matches
+     * @return  true if the original code value is returned as the result on a failed
+     *          search
      */
-    public void setSearchAllPipelines(boolean bool)
+    public boolean getOnSearchFailUseOrginal()
     {
-        this.searchAllPipelines = bool;
+        return this.onSearchFailUseOrginal;
     }
 
     /**
-     * If true all pipelines will be searched for a result, even after on has
-     * found a match
+     * DOCUMENT ME!
+     *
+     * @return
+     */
+    public List<SearchPipeline> getPipeLineList()
+    {
+        return this.pipeLineList;
+    }
+
+    /**
+     * If true all pipelines will be searched for a result, even after on has found a
+     * match.
      *
      * @return
      */
@@ -311,11 +299,31 @@ public class SearchProcessor
     }
 
     /**
-     * Determine if the original code value is returned as the result on a
-     * failed search
+     * DOCUMENT ME!
+     */
+    @PostConstruct
+    public void initialize()
+    {
+        searchCache = CacheBuilder.newBuilder().maximumSize(cacheSize)
+            .expireAfterWrite(10, TimeUnit.MINUTES).build();
+    }
+
+    /**
+     * DOCUMENT ME!
      *
-     * @param bool true if the original code value is returned as the result on
-     * a failed search
+     * @param  cacheSize
+     */
+    public void setCacheSize(int cacheSize)
+    {
+        this.cacheSize = cacheSize;
+    }
+
+    /**
+     * Determine if the original code value is returned as the result on a failed
+     * search.
+     *
+     * @param  bool  true if the original code value is returned as the result on a
+     *               failed search
      */
     public void setOnSearchFailUseOrginal(boolean bool)
     {
@@ -323,14 +331,93 @@ public class SearchProcessor
     }
 
     /**
-     * Determine if the original code value is returned as the result on a
-     * failed search
+     * DOCUMENT ME!
      *
-     * @return true if the original code value is returned as the result on a
-     * failed search
+     * @param  list
      */
-    public boolean getOnSearchFailUseOrginal()
+    public void setPipeLineList(List<SearchPipeline> list)
     {
-        return this.onSearchFailUseOrginal;
+        this.pipeLineList = list;
+    }
+
+    /**
+     * Controls if just the first pipeline to find a match should be consulted.
+     *
+     * @param  bool  true if all pipelines should be searched for matches
+     */
+    public void setSearchAllPipelines(boolean bool)
+    {
+        this.searchAllPipelines = bool;
+    }
+
+    /*
+     * public static CodeReference getCodeReference(String targetSystem, String
+     * sourceSystem, String sourceCode, String sourceText) { return
+     * getSerchProcessor().findCode(targetSystem, sourceSystem, sourceCode,
+     * sourceText);
+     *
+     * }
+     */
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   sourceSystem
+     * @param   sourceCode
+     * @param   sourceText
+     *
+     * @return
+     */
+    public CodeReference translateCode(String sourceSystem, String sourceCode,
+        String sourceText)
+    {
+        CodeReference out;
+
+        if (codeTargetMap.containsKey(sourceSystem))
+        {
+            out = this.findCode(codeTargetMap.get(sourceSystem), sourceSystem,
+                    sourceCode, sourceText);
+        }
+        else
+        {
+
+            // Refelect back out what we have.
+            out = new CodeReference(sourceSystem, sourceCode, sourceText);
+        }
+
+        return out;
+    }
+
+    class PipelineSearcher implements Callable<List<CodeReference>>
+    {
+        CodeSearch find;
+        List<SearchPipeline> pipelines;
+        boolean searchAllPipelines;
+
+        PipelineSearcher(CodeSearch find, List<SearchPipeline> pipelines,
+            boolean searchAllPipelines)
+        {
+            this.find = find;
+            this.pipelines = pipelines;
+            this.searchAllPipelines = searchAllPipelines;
+        }
+
+        @Override
+        public List<CodeReference> call()
+        {
+            LinkedList<CodeReference> out = new LinkedList<CodeReference>();
+            Iterator<SearchPipeline> itr = pipelines.iterator();
+
+            while (itr.hasNext())
+            {
+                SearchPipeline sp = itr.next();
+
+                if (sp.SupportsTarget(find))
+                {
+                    sp.match(find, out);
+                }
+            }
+
+            return out;
+        }
     }
 }
